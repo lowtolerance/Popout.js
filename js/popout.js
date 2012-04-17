231bb505599d5c25a090350fb45255e668b4642d
lowtolerance <robnixon515@gmail.com>
2012-04-17 19:22:02 -0400

    Switched to alternate, more complete version of Popout.js

--- js/popout.js
+++ js/popout.js
@@ -1,5 +1,5 @@
 //Determine our 4 corner coordinates based off the given dimensions and offsets.
-function getVals(width,height,left,top){
+function getCornerVals(width,height,left,top){
 	var corner = new Array (4);
 	for (i=0;i<4;i++)
 		corner[i] = new Array(2);
@@ -13,39 +13,45 @@ function getVals(width,height,left,top){
 	corner[3][1] = top+height;
 	return corner; 
 }
+
 //Retrieves and calculates dimensions and offsets of our divs as well as determine what
 //faces get drawn.
-function elem(nThis,mid) {
-	//determine the width of the 
+function getElemProperties(nThis,midPoint) {
+	//TODO-Look into replacing jQuery reliance.
 	this.width = Math.round($(nThis).outerWidth());
     this.height = Math.round($(nThis).outerHeight());
     offset = $(nThis).offset();
     this.left=offset.left;
     this.top=offset.top;
-    this.coord = getVals(this.width,this.height,this.left,this.top);
+    this.coord = getCornerVals(this.width,this.height,this.left,this.top);
    	this.popColor=$(nThis).css("background-color");
-   	if (this.coord[0][0] >= mid[0]) this.leftFace=true;
-   	if (this.coord[1][0] < mid[0]) this.rightFace=true;
-   	if (this.coord[0][1] > mid[1]) this.topFace=true;
-   	if (this.coord[3][1] <= mid[1]) this.bottomFace=true;
+	//Determine where corners are in relation to the mid point to
+	//determine what sides should be visible.
+   	if (this.coord[0][0] >= midPoint[0]) this.leftFace=true;
+   	if (this.coord[1][0] < midPoint[0]) this.rightFace=true;
+   	if (this.coord[0][1] > midPoint[1]) this.topFace=true;
+   	if (this.coord[3][1] <= midPoint[1]) this.bottomFace=true;
+	//Determine which corner is closest to the midpoint.
    	this.distance=99999999;
    	for (a=0;a<3;a++) {
-   		distBuff=Math.sqrt(Math.pow(mid[0]-this.coord[a][0],2) +Math.pow(mid[1]-this.coord[a][1],2));
+   		distBuff=Math.sqrt(Math.pow(midPoint[0]-this.coord[a][0],2) +Math.pow(midPoint[1]-this.coord[a][1],2));
    		if (distBuff < this.distance) this.distance = distBuff;
    	}  			
 }
 
 //Set up our canvas and draw each side. Additionally, set our gradient fill.
 function drawFace(coord, mid, popColor,gs,x1,x2,side) {
-	//Gradients in our case run either up/down or left right. W
+	//Gradients in our case run either up/down or left right.
 	//We have two algorithms depending on whether or not it's a sideways facing piece.
 	//Rather than parse the "rgb(r,g,b)" string(popColor) retrieved from elsewhere, it is simply
 	//offset with the gs variable to give the illusion that it starts at a darker color.
 	var canvas = document.getElementById('depth');
+	//This is for excanvas.js
 	var G_vmlCanvasManager;
 	if (G_vmlCanvasManager != undefined) { // ie IE
                 G_vmlCanvasManager.initElement(canvas);
         }
+	//Init canvas
 	if (canvas.getContext) {
 		var ctx = canvas.getContext('2d'); 
 		if (side) var lineargradient=ctx.createLinearGradient(coord[x1][0]+gs,mid[1],mid[0],mid[1]);
@@ -62,6 +68,8 @@ function drawFace(coord, mid, popColor,gs,x1,x2,side) {
 		ctx.fill();
 	}
 }
+
+//Pretty self-explanatory.
 function clearCanvas() {
 	var canvas = document.getElementById('depth');
 	var G_vmlCanvasManager;
@@ -71,25 +79,28 @@ function clearCanvas() {
 	if (canvas.getContext) {
 		var ctx = canvas.getContext('2d');
 		ctx.clearRect(0,0,$(document).width(),$(document).height());
-	
-	
 	}
 }
+
+//Sort our array of elements to be drawn using the Painter's algorithm.
+//Draws objects from furthest out to closest in.
 function sortByDistance(a,b) {
 	var x = a.distance;
 	var y = b.distance;
 	return ((x < y) ? 1 : ((x > y) ? -1  : 0));
 }
+
 //Our main function. Loops through each element given the "pop" class, and gets
 //necessary information: offset, dimensions, color, and the midpoint of the document.
 function draw(e) {
+	setTimeout(function() {
 		var arr = new Array()
 		var i = 0;
 		var mid = new Array(2);
 		$(".pop").each(function() {
 			mid[0]=Math.round($(document).width()/2);
 			mid[1]=Math.round($(document).height()/2);
-			arr[arr.length++]=new elem(this,mid);
+			arr[arr.length++]=new getElemProperties(this,mid);
 			i++;
 		});
 		arr.sort(sortByDistance);
@@ -102,7 +113,10 @@ function draw(e) {
 			//declare which corners make up our face, in this case the lower two corners, coord[3], and coord[2].
 			if (arr[a].bottomFace) drawFace(arr[a].coord,mid,arr[a].popColor,-60,3,2);
 			if (arr[a].topFace) drawFace(arr[a].coord,mid,arr[a].popColor,60,0,1);
+			if (arr[a].leftFace) drawFace(arr[a].coord,mid,arr[a].popColor,60,0,3,true);
+			if (arr[a].rightFace) drawFace(arr[a].coord,mid,arr[a].popColor,-60,1,2,true);
 		}
+	},1);
 }
 
 function getDocHeight() {
@@ -126,6 +140,9 @@ function getWidth(){
 }
 
 $(document).ready(function () {
+	//Inject our canvas into the "background".
+	//Excanvas doesn't like the way jQuery handles DOM elements, 
+	//so we're using straigh DOM methods.
 	d=document;
 	var w=d.getElementById("wrapper");
 	var divbg = d.createElement("div");
@@ -138,18 +155,20 @@ $(document).ready(function () {
 	divbg.appendChild(cvs);
 	w.parentNode.insertBefore(divbg,w);
 	
-	
-	
-//	$("#wrapper").before("<div id='background'><canvas id='depth' width='"+$(window).width()+"' height='"+$(document).height()+"'></canvas></div>");
+	//Hijax our links
 	$("a.pop").attr("href","#");
 	$("a.pop").click(function(){
 		$(".active").removeClass('active');
         $(this).addClass('active');
 		var activepage=$(".active").attr("id");
-		$("#content").load(activepage+".html #content");
+		$("#content").load(activepage+".html .loadme",draw);
 		draw();
 	});
+	//we need to refresh the background if colors change, such as with a hover event.
 	$("a.pop").bind("mouseenter mouseleave focusin focusout",draw);
+	//Our canvas needs to grown with the document.
+	//Again, excanvas.js doesn't like jQuery touching the DOM, so we're doing it the
+	//"hard" way.
 	$(window).resize(function() {
 		var size=new getWidth();
 		var depth=document.getElementById("depth");
